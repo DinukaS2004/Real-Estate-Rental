6c3f25bbd17a8a0ca717d72cfe41d7f0cca19c76
Dinuka Samarasinghe <dinukad735@gmail.com>
2026-05-03 13:57:28 +0530

    add HeroSection component with search functionality and animations

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 const HeroSection = () => {
   return (
     <div className="pt-[72px]">
@@ -10,8 +14,38 @@ const HeroSection = () => {
         className='object-cover object-center'
         priority
     />
-
+    <div className='absolute inset-0 bg-black bg-opacity-60'></div>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{duration:0.8}}
+      className="absolute top-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center w-full"
+    >
+      <div className='max-w-4xl mx-auto px-16 sm:px-12'>
+        <h1 className='text-5xl font-bold text-white mb-4'>Start your jounery to find your perfect rental</h1>
+      </div>
+      <p className="text-xl text-white mb-8">
+            Explore our wide range of rental properties tailored to fit your
+            lifestyle and needs!
+        </p>
+        <div className="flex justify-center">
+            <Input
+              type="text"
+              value="search"
+              onChange={() => {}}
+              placeholder="Search by city, neighborhood or address"
+              className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
+            />
+            <Button
+              onClick={() => {}}
+              className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
+            >
+              Search
+            </Button>
+          </div>
+    </motion.div>
     </div>
+    
   )
 }
 
